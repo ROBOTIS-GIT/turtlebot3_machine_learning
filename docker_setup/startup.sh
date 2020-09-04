f5b592bbfc46e90dab8edfe6fb3c10334cb44629
luwi1993 <lwidowski@alessio-analytics.com>
2020-09-04 10:18:24 +0200

    fix time sleep

--- docker_setup/startup.sh
+++ docker_setup/startup.sh
@@ -1,7 +1,7 @@
 echo "COMPOSE UP"
 sudo docker-compose up --scale ros_ml=2 &
 #echo SLEEP
-sleep 5
+sleep 3
 
 echo "SET VARIABLE"
 CONTAINERID=$(sudo docker ps | grep docker_setup_ros_ml| awk '{print $1}')
