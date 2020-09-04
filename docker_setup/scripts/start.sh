96e1bed09670d2cb05310c197d0f6e72922d4d5f
tnfru <lars.lmueller@gmail.com>
2020-09-04 20:30:18 +0200

    add: result graph

--- docker_setup/scripts/start.sh
+++ docker_setup/scripts/start.sh
@@ -3,4 +3,6 @@ roscore &
 sleep 2
 roslaunch turtlebot3_dqn turtlebot3_environment.launch stage:="$1" &
 sleep 2
-roslaunch turtlebot3_dqn turtlebot3_dqn.launch stage:="$1"
+roslaunch turtlebot3_dqn turtlebot3_dqn.launch stage:="$1" &
+sleep 2
+roslaunch turtlebot3_dqn result_graph.launch
