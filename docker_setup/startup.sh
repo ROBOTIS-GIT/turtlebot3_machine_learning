1f2b15250d054e9ada38531a6d9f3ef0b854a430
tnfru <lars.lmueller@gmail.com>
2020-09-04 20:32:46 +0200

    refactor: cleanup startup

--- docker_setup/startup.sh
+++ docker_setup/startup.sh
@@ -1,17 +1,8 @@
 echo "COMPOSE UP"
 sudo docker-compose up --scale ros_ml=2 &
-#echo SLEEP
 sleep 3
 
 echo "SET VARIABLE"
 CONTAINERID=$(sudo docker ps | grep docker_setup_ros_ml| awk '{print $1}')
 
 echo $CONTAINERID
-#echo "CHECK VALUE"
-#echo "ID:" > test.txt 
-#echo $CONTAINERID >> test.txt
-#cat test.txt
-
-#echo "EXEC"
-#sudo docker exec -it $CONTAINERID /bin/bash
-
