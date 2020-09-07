6953b171b832c2a7406549fb1788648a8f0330d0
luwi1993 <lwidowski@alessio-analytics.com>
2020-09-07 13:00:57 +0200

    implement her

--- turtlebot3_dqn/nodes/hindsight_experience_replay.py
+++ turtlebot3_dqn/nodes/hindsight_experience_replay.py
@@ -1,13 +1,16 @@
 from collections import deque
 import random
+
 class HindsightExperienceReplay:
     def __init__(self, maxlen=1000000, batch_size=64):
         self.memory = self.memory = deque(maxlen=maxlen)
         self.n_entrys = 0
         self.batch_size = batch_size
 
-    def append_memory(self, state, action, goal, reward, next_state, done):
+    def append_memory(self, state, action, goal, her_goal, reward, next_state, done):
         self.memory.append((state, action, goal, reward, next_state, done))
+        self.memory.append((state, action, her_goal, 200, next_state, done))
+        self.n_entrys += 2
 
     def sample(self):
-        mini_batch = random.sample(self.memory, self.batch_size)
+        return random.sample(self.memory, self.batch_size)
