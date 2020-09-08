ebe16fd19fdb083a23725f62b3d576001cd57fe2
luwi1993 <lwidowski@alessio-analytics.com>
2020-09-08 21:16:40 +0200

    fix volume to start bash

--- docker_setup/docker_start_container.sh
+++ docker_setup/docker_start_container.sh
@@ -1,2 +1,2 @@
-sudo docker run -it --env="DISPLAY" --env="QT_X11_NO_MITSHM=1" --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" --volume="./cfg/:/root/cfg:rw" docker_setup_ros_ml
+sudo docker run -it --env="DISPLAY" --env="QT_X11_NO_MITSHM=1" --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" --volume="/home/lwidowski/RoboticsProjects/catkin_ws/src/turtlebot3_machine_learning/docker_setup/cfg/:/root/cfg:rw" docker_setup_ros_ml
 
