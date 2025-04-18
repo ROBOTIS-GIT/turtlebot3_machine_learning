1a6cf4b01cb28bf6fdf48f55f47a9cb2d15dbafc
chan-1207 <dddoggi1207@gmail.com>
2025-04-18 10:43:52 +0900

    Fix typo

--- turtlebot3_dqn/turtlebot3_dqn/dqn_test.py
+++ turtlebot3_dqn/turtlebot3_dqn/dqn_test.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-#
+#################################################################################
 # Copyright 2019 ROBOTIS CO., LTD.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,6 +13,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#################################################################################
 #
 # Authors: Ryan Shim, Gilbert, ChanHyeong Lee
 
